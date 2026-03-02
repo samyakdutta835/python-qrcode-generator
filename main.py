9dfc798470495db5385c0b3ac637f46e5c1eac93
samyakdutta835 <samyakdutta835@gmail.com>
2026-03-02 12:13:34 +0530

    Added sample QR output

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import qrcode as qr
 from PIL import Image
 
-qr = qr.QRCode(version=1,
-               error_correction=qr.constants.ERROR_CORRECT_H,
-               box_size=10,
-               border=5)
-qr.add_data("https://www.youtube.com/watch?v=FOGRHBp6lvM&list=PLjVLYmrlmjGfAUdLiF2bQ-0l8SwNZ1sBl&index=1")
+
+qr = qr.QRCode(
+    version=1,
+    error_correction=qr.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=5
+)
+
+qr.add_data("https://github.com/samyakdutta835")  # my github link
 qr.make(fit=True)
-img = qr.make_image(fill_color="black", back_color="white")
+
+img = qr.make_image(
+    fill_color="black",
+    back_color="white"
+)
+
 img.save("qr_code.png")
